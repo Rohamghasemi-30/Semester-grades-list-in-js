b71a7292820c408d0e93d69f0b05f7787a9a408e
Roham ghasemi <ghasemiroham71@gmail.com>
2024-03-01 09:59:26 +0330

    finished-js

---
+++ js/app.js
@@ -0,0 +1,109 @@
+
+// first term
+
+let mohammadEftekhari = {
+    name: "mohammadEftekhari",
+    classActivity: "C+",
+    handout: "A-",
+    exam: "C+",
+    overal: "B+",
+    Presence: false,
+};
+
+let aboulfazlEbrahimi = {
+    name: "aboulfazlEbrahimi",
+    classActivity: "C+",
+    handout: "C-",
+    exam: "C+",
+    overal: "C+",
+    Presence: true,
+};
+
+let  yasinKhoshbakht = {
+    name: "yasinKhoshbakht",
+    classActivity: "A-",
+    handout: "A-",
+    exam: "B+",
+    overal: "A-",
+    Presence: true,
+};
+
+let rohamGhasemi = {
+    name: "Rohamghasemi",
+    classActivity: "A+",
+    handout: "B+",
+    exam: "A-",
+    overal: "A-",
+    Presence: true,
+};
+
+let parsaVahid = {
+    name: "parsaVahid",
+    classActivity: "B-",
+    handout: "C-",
+    exam: "C-",
+    overal: "C-",
+    Presence: false,
+};
+
+let kasraKeifi = {
+    name: "kasraKeifi",
+    classActivity: "C+",
+    handout: "C-",
+    exam: "C-",
+    overal: "C-",
+    Presence: false,
+};
+
+
+// first term
+
+
+
+
+// second semester
+
+let parhamSobhaniSecondSemester = {
+    name: "parhamSobhani",
+    classActivity: "C+",
+    handout: "C+",
+    exam: "C+",
+    overal: "C+",
+    Presence: true,
+};
+
+let aboulfazlEbrahimiSecondSemester = {
+    name: "aboulfazlEbrahimi",
+    classActivity: "C+",
+    handout: "C+",
+    exam: "B-",
+    overal: "B-",
+    Presence: true,
+};
+
+let  yasinKhoshbakhtSecondSemester = {
+    name: "yasinKhoshbakht",
+    classActivity: "A-",
+    handout: "A+",
+    exam: "A+",
+    overal: "A+",
+    Presence: true,
+};
+
+let rohamGhasemiSecondSemester = {
+    name: "Rohamghasemi",
+    classActivity: "A+",
+    handout: "A+",
+    exam: "A+",
+    overal: "A+",
+    Presence: true,
+};
+
+// second semester
+
+
+let firstSemesterClassList =[mohammadEftekhari, aboulfazlEbrahimi, yasinKhoshbakht, rohamGhasemi, parsaVahid, kasraKeifi];
+let secondSemesterClassList =[parhamSobhaniSecondSemester, aboulfazlEbrahimiSecondSemester, yasinKhoshbakhtSecondSemester, rohamGhasemiSecondSemester];
+
+
+console.log(firstSemesterClassList,secondSemesterClassList)
